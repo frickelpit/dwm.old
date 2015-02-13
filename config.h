8454bf5984dbcd267e3f415f0179fb565abf90a8
Peter S <pit.invisible@gmail.com>
2015-02-13 22:35:03 +0100

    added some rules

--- config.h
+++ config.h
@@ -27,9 +27,14 @@ static const Bool clicktofocus      		= True;     	/* Change focus only on click
 static const char *tags[] = { "web", "chat|music", "mail|news", "other" };
 
 static const Rule rules[] = {
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
-	{ "Firefox",  NULL,       NULL,       1,            False,       -1 },
+	/* class	instance	title		tags mask	isfloating	monitor */
+	{ "Firefox",	NULL,		NULL,		1,		False,		-1 },
+	{ NULL,		NULL,		"mutt",		1 << 2,		False,		-1 },
+	{ NULL,		NULL,		"newsbeuter",	1 << 2,		False,		-1 },
+	{ NULL,		NULL,		"irssi",	1 << 1,		False,		-1 },
+	{ NULL,		NULL,		"ncmpcpp",	1 << 1,		False,		-1 },
+	{ NULL,		NULL,		"jabber",	1 << 1,		False,		-1 },
+	{ NULL,		NULL,		"mc",		1 << 2,		False,		-1 },
 };
 
 /* layout(s) */
